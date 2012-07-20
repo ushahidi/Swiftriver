f020752a00be8f5d595c3e19e6bf862f7080619a
Emmanuel Kala <emkala@gmail.com>
2012-07-20 18:34:08 +0300

    Naming convention update
    * Suffix the update script with the name of the affected tables or
    relevant subject

--- application/classes/controller/river/filters.php
+++ application/classes/controller/river/filters.php
@@ -9,11 +9,11 @@
  * LICENSE: This source file is subject to GPLv3 license 
  * that is available through the world-wide-web at the following URI:
  * http://www.gnu.org/copyleft/gpl.html
- * @author	   Ushahidi Team <team@ushahidi.com> 
- * @package	   SwiftRiver - http://github.com/ushahidi/Swiftriver_v2
+ * @author     Ushahidi Team <team@ushahidi.com> 
+ * @package    SwiftRiver - http://github.com/ushahidi/Swiftriver_v2
  * @subpackage Controllers
  * @copyright  Ushahidi - http://www.ushahidi.com
- * @license	   http://www.gnu.org/copyleft/gpl.html GNU General Public License v3 (GPLv3) 
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License v3 (GPLv3) 
  */
 
 class Controller_River_Filters extends Controller_River_Settings {
